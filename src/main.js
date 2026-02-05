c182ade95a18adc6abd51311b4db540f8a71445a
MiyanMei0001 <miyanmei0001@gmail.com>
2026-02-05 14:42:18 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-    const targetUrl = 'https://yan-montelli.vercel.app' + url.pathname + url.search;
-    
-    return fetch(targetUrl, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-    });
-  },
-};
+    const { searchParams } = new URL(request.url)
+    const verifyUrl = searchParams.get("url")
+
+    if (!verifyUrl) {
+      return new Response("Missing url", { status: 400 })
+    }
+
+    const res = await fetch("https://safelinku.com/api/v1/links", {
+      method: "POST",
+      headers: {
+        "Authorization": "65b295c090ecfd2971de2de476835f845d026414",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        url: verifyUrl
+      })
+    })
+
+    const data = await res.json()
+
+    if (!data.url) {
+      return new Response("Failed to generate link", { status: 502 })
+    }
+
+    return new Response(JSON.stringify({
+      shortlink: data.url
+    }), {
+      headers: { "Content-Type": "application/json" }
+    })
+  }
+}
